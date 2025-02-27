b688592fc44fcb41972672b904b1272489431dcc
Daniel Servejeira <servejeiradaniel@gmail.com>
2025-02-27 11:42:03 -0300

    fix: database path

--- SEProject/src/seproject/persistence/Persistence.java
+++ SEProject/src/seproject/persistence/Persistence.java
@@ -12,8 +12,13 @@ import java.io.*;
  */
 public class Persistence {
 
+    private static final String BASE_PATH = "src/seproject/database/";
+
     public static <T extends Serializable> void saveArchive(T object, String fileName) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
+        File dir = new File(BASE_PATH);
+        if (!dir.exists()) dir.mkdirs();
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(BASE_PATH + fileName))) {
             oos.writeObject(object);
         } catch (IOException e) {
             System.err.println("Error saving: " + e.getMessage());
@@ -22,10 +27,10 @@ public class Persistence {
 
     @SuppressWarnings("unchecked")
     public static <T extends Serializable> T loadArchive(String fileName, T defaultValue) {
-        File file = new File(fileName);
+        File file = new File(BASE_PATH + fileName);
         if (!file.exists()) return defaultValue;
 
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
             return (T) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
             System.err.println("Error loading: " + e.getMessage());
